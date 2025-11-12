2a13f4bde237879b684da7f2763bb0d45de72f53
Vijay Jadhav <mail2vjadhav@gmail.com>
2025-11-12 12:59:21 +0000

    CCL-5869-albs: Fix ALB creation

--- modules/ingress/route53-public-zone/main.tf
+++ modules/ingress/route53-public-zone/main.tf
@@ -19,17 +19,26 @@ resource "time_sleep" "wait_30_seconds" {
 
 # Create Route53 A Record Alias for External ALB (only when external_ingress = true)
 resource "aws_route53_record" "external_alb" {
-  count   = var.external_ingress ? 1 : 0
+  count   = (var.external_ingress && var.alb_dns_ready) ? 1 : 0
+
   zone_id = var.zone_id
   name    = "*.${var.domain_name}"
   type    = "A"
 
   alias {
-    name                   = var.external_alb_dns
-    zone_id                = var.alb_hosted_zone_id
+    name                   = var.external_alb_dns        # e.g. my-alb-123.eu-west-1.elb.amazonaws.com
+    zone_id                = var.alb_hosted_zone_id      # e.g. data.aws_lb.external.zone_id
     evaluate_target_health = true
   }
+
+  lifecycle {
+    precondition {
+      condition     = var.external_alb_dns != "" && var.alb_hosted_zone_id != ""
+      error_message = "ALB DNS name/hosted zone ID must be set when creating the external ALB record."
+    }
+  }
 }
+
 
 
 resource "aws_route53_record" "acm_validation" {
